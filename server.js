5b2717a4b0385a67a9c2e3c43ff0551636a4b37a
billtthutao <billtt@163.com>
2018-06-17 20:26:14 +0800

    Add project page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/about',(request,response) => {
   });
 });
 
+app.get('/projects',(request,response) =>{
+  response.render('projects.hbs',{pageTitle:'Some projects', 
+                                  pageHeader:'Projects'
+  });
+});
+
 app.get('/bad',(request,response) => {
   response.send({
     errorMessage: 'Unable to handle the request'
